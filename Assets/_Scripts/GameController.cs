61ba17b6de52c9f5c717bd93fd2c6f0d136f3dcf
Arlina Ramrattan <arlina.ramrattan@gmail.com>
2016-11-26 12:29:39 -0500

    First commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	//Private Instance Variables
+	private int _livesValue = 100;
+	private int score = 0;
+
+	//Public Instance Variables
+	[Header("Labels")]
+	public Text livesLabel;
+	public Text scoreLabel;
+	public Text gameOverLabel;
+	public Text finalLabel;
+	public Text victory;
+	public Button restartButton;
+
+	[Header("Game Objects")]
+	public GameObject Dementor;
+	public GameObject Beans;
+	public GameObject Player;
+	public GameObject tree1;
+	public GameObject tree2;
+
+	[Header("Music")]
+	public AudioSource endMusic;
+	//public AudioSource backgroundMusic;
+
+	//Public Properties
+
+	public int scoreValue // updates score
+	{
+		get { return this.score; }
+		set
+		{
+			this.score = value;
+			this.scoreLabel.text = "Score: " + this.score;
+		}
+	}
+
+	public int livesValue //updates lives
+	{
+		get { return this._livesValue; }
+		set
+		{
+			this._livesValue = value;
+			this.livesLabel.text = "Health: " + this._livesValue;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//this._livesValue = 100;
+		//this.score = 0;
+		this.Dementor.SetActive(true);
+		this.gameOverLabel.gameObject.SetActive(false);
+		this.restartButton.gameObject.SetActive (false);
+		this.victory.gameObject.SetActive (false);
+		this.finalLabel.gameObject.SetActive (false);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}    
+
+	public void endGame()
+	{
+		this.scoreLabel.gameObject.SetActive(false);
+		this.livesLabel.gameObject.SetActive(false);
+		this.gameOverLabel.gameObject.SetActive(true);
+		this.finalLabel.gameObject.SetActive(true);
+		this.finalLabel.text = "Score: " + this.score;
+		this.restartButton.gameObject.SetActive(true);
+		this.Dementor.SetActive(false);
+		this.Beans.SetActive(false);
+		this.Player.SetActive(false);
+		endMusic.Play();
+		endMusic.loop = true;
+	}
+	public void endGameVictory(){
+		this.scoreLabel.gameObject.SetActive(false);
+		this.livesLabel.gameObject.SetActive(false);
+		this.victory.gameObject.SetActive(true);
+		this.finalLabel.gameObject.SetActive(true);
+		this.finalLabel.text = "Score: " + this.score;
+		this.restartButton.gameObject.SetActive(true);
+		this.Dementor.SetActive(false);
+		this.Beans.SetActive(false);
+		this.Player.SetActive(false);
+		endMusic.Play();
+		endMusic.loop = true;
+	}
+
+	public void restart_click()
+	{
+		SceneManager.LoadScene("Main");
+	}
+}
---
+++ Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerScript : MonoBehaviour {
+
+    //Public Instance Variables
+	public GameController gameController;
+
+	[Header("Game Objects")]
+    public GameObject Dementor;
+    public GameObject Beans;
+    public GameObject tree1;
+    public GameObject tree2;
+
+    [Header("Music")]
+
+    public AudioSource backgroundMusic;
+
+
+    [Header("Sounds")]
+    public AudioSource beans;
+    public AudioSource dementor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        backgroundMusic.Play();
+        backgroundMusic.loop = true;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider otherGameObject)
+    {
+        if (otherGameObject.CompareTag("Beans")) 
+        {
+            otherGameObject.gameObject.SetActive(false);
+			this.gameController.scoreValue += 10;
+            this.beans.Play();
+			Debug.Log ("B Hit");
+        }
+
+        if (otherGameObject.gameObject.name == "Tree") //cheat to transport to other tree
+        {
+            transform.position = new Vector3(tree2.gameObject.transform.position.x, tree2.gameObject.transform.position.y + 5, tree2.gameObject.transform.position.z - 3);
+        }
+
+        if (otherGameObject.gameObject.name == "Tree 1")//cheat to transport back to other tree, but on the top of the maze
+        {
+            transform.position = new Vector3(tree1.gameObject.transform.position.x, tree1.gameObject.transform.position.y + 5, tree1.gameObject.transform.position.z - 3);
+        }
+
+		if (otherGameObject.CompareTag("Enemy")) 
+        {
+            this.gameController.livesValue -= 5;
+			Debug.Log ("D hit");
+			this.dementor.Play();
+
+			if (this.gameController.livesValue <= 0)
+            {
+                this.backgroundMusic.Stop();
+                this.backgroundMusic.loop = false;
+                this.gameController.endGame();
+            }
+
+        }
+		if (otherGameObject.CompareTag("Cup")) 
+		{
+
+				this.backgroundMusic.Stop();
+				this.backgroundMusic.loop = false;
+				this.gameController.endGameVictory();
+			
+
+		}
+    }
+		
+}
